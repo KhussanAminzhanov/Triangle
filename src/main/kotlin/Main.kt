30e2156794ad34648540a2c9ae458eb16a033faa
Husan Amizhanov <aminjanov.husan@jits-3-13.jits.ipa.dc>
2022-04-19 15:44:48 +0600

    add rotate function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,4 @@
-import kotlin.math.abs
-import kotlin.math.pow
-import kotlin.math.roundToInt
-import kotlin.math.sqrt
+import kotlin.math.*
 
 data class Vector(var x: Double, var y: Double)
 
@@ -25,15 +22,14 @@ class Triangle(first: Vector, second: Vector, third: Vector) {
             throwExceptionIfCollinear()
         }
 
-    private var lengthA = getDistance(first, second)
-    private var lengthB = getDistance(second, third)
-    private var lengthC = getDistance(first, third)
-
     val perimeter: Double
-        get() = ((lengthA + lengthB + lengthC) * 100).roundToInt() / 100.0
+        get() = ((getDistance(first, second) + getDistance(second, third) + getDistance(first, third)) * 100).roundToInt() / 100.0
 
     val area: Double
         get() = abs(0.5 * (first.x * (second.y - third.y) + second.x * (third.y - first.y) + third.x * (first.y - second.y)))
+
+    val center: Vector
+        get() = Vector((first.x + second.x + third.x) / 3, (first.y + second.y + third.y) / 3)
 
     init {
         if (isCollinear()) throwExceptionIfCollinear()
@@ -51,6 +47,31 @@ class Triangle(first: Vector, second: Vector, third: Vector) {
         if (isCollinear()) throw java.lang.IllegalArgumentException()
     }
 
+    fun rotate(a: Double) {
+        val angle = Math.toRadians(a)
+        val x1 = first.x
+        val x2 = second.x
+        val x3 = third.x
+        val y1 = first.y
+        val y2 = second.y
+        val y3 = third.y
+        val centerX = center.x
+        val centerY = center.y
+
+        val x1r = ((x1 - centerX) * cos(angle) - (y1 - centerY) * sin(angle) + centerX)
+        val y1r = ((x1 - centerX) * sin(angle) + (y1 - centerY) * cos(angle) + centerY)
+
+        val x2r = ((x2 - centerX) * cos(angle) - (y2 - centerY) * sin(angle) + centerX)
+        val y2r = ((x2 - centerX) * sin(angle) + (y2 - centerY) * cos(angle) + centerY)
+
+        val x3r = ((x3 - centerX) * cos(angle) - (y3 - centerY) * sin(angle) + centerX)
+        val y3r = ((x3 - centerX) * sin(angle) + (y3 - centerY) * cos(angle) + centerY)
+
+        first = Vector(x1r, y1r)
+        second = Vector(x2r, y2r)
+        third = Vector(x3r, y3r)
+    }
+
     override fun toString(): String {
         return "First vector: x = ${first.x}, y = ${first.y}\n" +
                 "Second vector: x = ${second.x}, y = ${second.y}\n" +
@@ -59,11 +80,16 @@ class Triangle(first: Vector, second: Vector, third: Vector) {
 }
 
 fun main() {
-    val triangle = Triangle(Vector(-2.0, 7.0), Vector(1.0, 1.0), Vector(-3.0, 3.0))
+    val triangle = Triangle(Vector(1.0, 1.0), Vector(1.0, 3.0), Vector(3.0, 1.0))
 
     println(triangle)
     println("Perimeter: ${triangle.perimeter}")
     println("Area: ${triangle.area}")
+    println("Center: ${triangle.center}")
 
-    triangle.second = Vector(-2.0, 7.0)
+    //triangle.rotate(90.0)
+
+    //println(triangle)
+
+//    triangle.second = Vector(-2.0, 7.0)
 }
