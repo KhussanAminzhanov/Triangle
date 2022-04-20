e0dd4f22c3b6e56dbb3c77f8a11ddd7832f5af08
Husan Amizhanov <aminjanov.husan@jits-3-13.jits.ipa.dc>
2022-04-20 10:41:27 +0600

    remove rotate function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -47,31 +47,6 @@ class Triangle(first: Vector, second: Vector, third: Vector) {
         if (isCollinear()) throw java.lang.IllegalArgumentException()
     }
 
-    fun rotate(a: Double) {
-        val angle = Math.toRadians(a)
-        val x1 = first.x
-        val x2 = second.x
-        val x3 = third.x
-        val y1 = first.y
-        val y2 = second.y
-        val y3 = third.y
-        val centerX = center.x
-        val centerY = center.y
-
-        val x1r = ((x1 - centerX) * cos(angle) - (y1 - centerY) * sin(angle) + centerX)
-        val y1r = ((x1 - centerX) * sin(angle) + (y1 - centerY) * cos(angle) + centerY)
-
-        val x2r = ((x2 - centerX) * cos(angle) - (y2 - centerY) * sin(angle) + centerX)
-        val y2r = ((x2 - centerX) * sin(angle) + (y2 - centerY) * cos(angle) + centerY)
-
-        val x3r = ((x3 - centerX) * cos(angle) - (y3 - centerY) * sin(angle) + centerX)
-        val y3r = ((x3 - centerX) * sin(angle) + (y3 - centerY) * cos(angle) + centerY)
-
-        first = Vector(x1r, y1r)
-        second = Vector(x2r, y2r)
-        third = Vector(x3r, y3r)
-    }
-
     override fun toString(): String {
         return "First vector: x = ${first.x}, y = ${first.y}\n" +
                 "Second vector: x = ${second.x}, y = ${second.y}\n" +
@@ -85,11 +60,5 @@ fun main() {
     println(triangle)
     println("Perimeter: ${triangle.perimeter}")
     println("Area: ${triangle.area}")
-    println("Center: ${triangle.center}")
 
-    //triangle.rotate(90.0)
-
-    //println(triangle)
-
-//    triangle.second = Vector(-2.0, 7.0)
 }
